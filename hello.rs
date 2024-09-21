9368c4e4c4176c457ff407cf0d7459491edfc402
Iboro2 <nkereuwememmanueliboro05@gmail.com>
2024-09-21 18:15:50 +0100

    first commit

---
+++ hello.rs
@@ -0,0 +1,32 @@
+// fn main(){
+//     println!("Hello, World")
+// }
+
+// Integers
+// fn main() {
+//     let v: i32 = -43;
+//     let u: u64 = 100;
+//     println!("Signed Integer: {}", v);
+//     println!("Unsigned Integer: {}", u);
+// }
+
+
+// Floats
+// fn main() {
+//     let pi: f64 = 3.14;
+//     println!("Value of pi: {}", pi);
+// }
+
+
+// Boolean 
+// fn main(){
+//     let is_showing: bool = false;
+//     println!("Is it snowing? {}", is_showing)
+// }
+
+
+// Charachter Type - char
+fn main() {
+    let letter: char = 'r';
+    println!("This is a character: {}", letter);
+}
